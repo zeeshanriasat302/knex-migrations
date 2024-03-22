e74a5cb053d280c2c4c10c55279af48a5c55e25a
zeeshanriasatpl <zeehan.riasat@planlabsolutions.com>
2024-03-22 13:05:48 +0500

    knex intial commit

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+    development: {
+       client: 'mysql',
+       connection: {
+         host: 'localhost',
+         user: 'user',
+         password: 'mysql@root@pass123',
+         database: 'testmigrations',
+         port: 3306
+       },
+       migrations: {
+         tableName: 'knex_migrations',
+         directory: './migrations'
+       }
+    }
+   };
+   
+  
